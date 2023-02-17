d264da316df459292fb77bd99a32bef02d315d4e
tttthuy00 <thanhthuy280902@gmail.com>
2023-02-17 13:39:25 +0700

    hello

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = ()=>{
+    return(
+        <Hello/>
+    );
+}
+
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <View>
+      <Text style={styles.text1}>Hello</Text>
+    </View>
+  )
+}
+
+export default Hello
+
+const styles = StyleSheet.create({
+  text1:{
+    fontSize:30,
+    color:'pink',
+    fontWeight:'700'
+  }
+})
